02fc70d1f86c5a97b03c23608941d8039fe50a81
edw <w.edelbacher@vogelbusch-biopharma.com>
2025-08-01 14:09:39 +0200

    code eingetragen

--- bitcoin_pv_mining/your_code/utils.py
+++ bitcoin_pv_mining/your_code/utils.py
@@ -1,1 +1,8 @@
 # Rentabilitätsberechnungen
+def calc_miner_profitability(hashrate_th, verbrauch_kwh, btc_price_eur, kest, stromkosten):
+    daily_income = hashrate_th * 0.000008 * btc_price_eur
+    daily_cost = verbrauch_kwh * 24 * stromkosten
+    return (daily_income * (1 - kest)) - daily_cost
+
+def prozentwert_fuer_leistung(max_leistung_kwh, verfuegbar_kw):
+    return min(100, int((verfuegbar_kw / max_leistung_kwh) * 100))
